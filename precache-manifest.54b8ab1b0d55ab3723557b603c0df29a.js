f5a745c6b2e13f673f0f46be77b1477d9b1076f9
daniel <danielvo1510@gmail.com>
2020-05-12 21:37:49 +0800

    Updates

---
+++ precache-manifest.54b8ab1b0d55ab3723557b603c0df29a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "750076ea0820744b5f21f688d2825248",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9b46c2351fa32b9c31ca",
+    "url": "/monsters-rolodex/static/css/main.2f1389d8.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b46c2351fa32b9c31ca",
+    "url": "/monsters-rolodex/static/js/main.788d370e.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
